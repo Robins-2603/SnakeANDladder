2ea2be11ccd617c84f6cdf4a9e4d864a33d22c37
Robins-2603 <robinsforwork@gmail.com>
2022-06-21 00:13:14 +0530

    Adding: snake and ladder introduction

---
+++ src/com/snake_ladder/Game.java
@@ -0,0 +1,11 @@
+package com.snake_ladder;
+
+public class Game {
+    public static void  main(String[] args) {
+        System.out.println("Welcome to Snake & Ladder game");
+        // assigning player1 at initial position 0
+        int player1 = 0;
+        System.out.println("player1 standing at " + player1);
+
+    }
+}
